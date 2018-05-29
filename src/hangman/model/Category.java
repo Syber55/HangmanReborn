dd9836a90466e53eb8eaf743e18a727253f51a32
Syber55 <sajib_causevic@hotmail.com>
2018-05-29 22:50:47 +0200

    još promjena

---
+++ src/hangman/model/Category.java
@@ -0,0 +1,38 @@
+package hangman.model;
+
+import java.util.ArrayList;
+
+public class Category {
+
+	int categoryID;
+	String categoryName;
+	ArrayList<Category> categories = new ArrayList<>();
+
+	public Category() {
+	}
+
+	public int getCategoryID() {
+		return categoryID;
+	}
+
+	public void setCategoryID(int categoryID) {
+		this.categoryID = categoryID;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+	public ArrayList<Category> getCategories() {
+		return categories;
+	}
+
+	public void setCategories(ArrayList<Category> categories) {
+		this.categories = categories;
+	}
+
+}
--- src/hangman/model/User.java
+++ src/hangman/model/User.java
@@ -1,5 +1,46 @@
 package hangman.model;
+import java.util.ArrayList;
 
 public class User {
+	int userID;
+	String username;
+	String password;
+	ArrayList<User> users = new ArrayList<>();
+	
+	public User() {
+	}
 
+	public int getUserID() {
+		return userID;
+	}
+
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public ArrayList<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(ArrayList<User> users) {
+		this.users = users;
+	}
+	
+	
 }
