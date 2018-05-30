7e1823b25d8a05a703330f6095dbb74b4c2d317a
Syber55 <sajib_causevic@hotmail.com>
2018-05-30 12:59:19 +0200

    mali dodaci

--- src/hangman/model/Score.java
+++ src/hangman/model/Score.java
@@ -1,5 +1,16 @@
 package hangman.model;
 
 public class Score {
-
+ int score;
+ 
+ public Score() {
+ }
+ 
+ public int getScore() {
+	 return this.score;
+ }
+ 
+ public void setScore(int score) {
+	 
+ }
 }
