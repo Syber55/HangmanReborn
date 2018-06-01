1fd96471d83471388b35ce48efd681aa98208319
Zeljko <zeljkod93@live.com>
2018-06-01 19:08:13 +0200

    leaderboard jsp and servlet

--- WebContent/leaderboard.jsp
+++ WebContent/leaderboard.jsp
@@ -1,8 +1,10 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
 	pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 
-<%@ page import="java.sql.*"%>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<%@ page import="hangman.DAO.ScoreDAOImplementation"%>
+
+<!DOCTYPE html >
 <html>
 <head>
 <link rel="stylesheet" type="text/css" href="css/style.css">
@@ -16,24 +18,29 @@
 			<div class="leftPanel">
 
 				<div style="padding-left: 315px" id="leaderboard">
-					<table style="width: 100%">
-						<tr>
+					<h1>Scoreboard</h1>
+					<div class="score">
+						<table align="center" color="white">
+							<th></th>
 							<th>Username</th>
 							<th>Score</th>
-							
-						</tr>
-						<tr>
-							<td>Jill</td>
-							<td>Smith</td>
-							
-						</tr>
-						<tr>
-							<td>Eve</td>
-							<td>Jackson</td>
-							
-						</tr>
-					</table>
+							<c:forEach items="${scoreList}" var="game" varStatus="counter"
+								begin="1" end="10">
 
+
+								<tr>
+									<td>${counter.count}</td>
+									<td>${game.username}</td>
+									<td>${game.score}</td>
+									<!-- <td>${game.id}</td> -->
+
+
+
+
+								</tr>
+							</c:forEach>
+						</table>
+					</div>
 
 				</div>
 
--- src/hangman/servlets/LeaderBoardServlet.java
+++ src/hangman/servlets/LeaderBoardServlet.java
@@ -1,11 +1,17 @@
 package hangman.servlets;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import hangman.DAO.ScoreDAOImplementation;
+import hangman.model.Score;
 
 /**
  * Servlet implementation class LeaderBoardServlet
@@ -13,20 +19,31 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/LeaderBoardServlet")
 public class LeaderBoardServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LeaderBoardServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	public LeaderBoardServlet() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		ScoreDAOImplementation score = new ScoreDAOImplementation();
+
+		ArrayList<Score> scoreList;
+		try {
+			scoreList = score.getScores();
+
+			request.setAttribute("scoreList", scoreList);
+
+			request.getRequestDispatcher("leaderboard.jsp").forward(request, response);
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
