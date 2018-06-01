5a2723c49b1c0a00c52e9390b6a0c79d1e4a657b
Syber55 <sajib_causevic@hotmail.com>
2018-06-01 12:35:18 +0200

    login controller fertig

--- src/hangman/controller/LoginController.java
+++ src/hangman/controller/LoginController.java
@@ -1,9 +1,20 @@
 package hangman.controller;
 
+import java.util.ArrayList;
+
+import hangman.model.User;
+
 public class LoginController {
 
 	public static  boolean isUsernameAndPasswordCorrect(String username, String password) {
-		//Povlaèi arraylistu iz user klase i provjerava da li korisnik postoji, ako postoji onda provjerava da li je šifra uredu
+		User user = new User();
+		for (int i=0; i<user.users.size(); i++) {
+			if(username.equals(user.users.get(i).username)) {
+				if(password.equals(user.users.get(i).password)) {
+					return true;
+				}
+			}
+		}
 		return false;
 	}
 }
