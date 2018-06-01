b9e4c5af0a1dcfd686d1134656824699e473ffe4
EminaNuhic <36200390+EminaNuhic@users.noreply.github.com>
2018-06-01 02:17:19 +0200

    Update LoginController.java

--- src/hangman/controller/LoginController.java
+++ src/hangman/controller/LoginController.java
@@ -3,7 +3,22 @@ package hangman.controller;
 public class LoginController {
 
 	public static boolean isUsernameAndPasswordCorrect(String username, String password) {
-		//PovlaËi arraylistu iz user klase i provjerava da li korisnik postoji, ako postoji onda provjerava da li je öifra uredu
-		return false;
+		//Povlaƒçi arraylistu iz user klase i provjerava da li korisnik postoji, ako postoji onda provjerava da li je ≈°ifra uredu
+		 
+		ArrayList<User> users = new ArrayList<>();
+
+		if (users.contains(username)) {
+			if (password.length() > 8) {
+				for (int i = 0; i < password.length(); i++) {
+					if (Character.isUpperCase(password.charAt(i)))
+						if (Character.isDigit(password.charAt(i)))
+
+							System.out.println("User found");
+				}
+			}
+		} else {
+			System.out.println("User not found");
+		}
+	return false;
 	}
 }
